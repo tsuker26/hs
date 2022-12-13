326d83543319e80476c78274b68b79166067b871
igor <igorek888rane@gmail.com>
2022-12-13 09:59:14 +0300

    dateOutCount

--- src/pages/HotelSearchPage.jsx
+++ src/pages/HotelSearchPage.jsx
@@ -4,16 +4,15 @@ import Header from '../components/Header/Header'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { getHotelsAc } from '../redux/reducers/hotelsReducer'
+import { dateOutCount } from '../utils/dateOutCount'
 
 const HotelSearchPage = () => {
 	const dispatch = useDispatch()
 	const { locationInput, dateInput, countDayInput } = useSelector(
 		state => state.search
 	)
-	const setDay = new Date(dateInput).setDate(
-		new Date(dateInput).getDate() + +countDayInput
-	)
-	const dateOut = new Date(setDay).toISOString().split('T')[0]
+
+	const dateOut = dateOutCount(dateInput, countDayInput)
 
 	useEffect(() => {
 		dispatch(getHotelsAc({ locationInput, dateInput, dateOut }))
