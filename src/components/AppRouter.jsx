b25b166aa9043209287390854f8faf81c5bdf4c5
igor <igorek888rane@gmail.com>
2022-12-27 12:27:32 +0300

    Роутинг

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import RequireAuth from '../hoc/RequireAuth'
+import AuthPage from '../pages/AuthPage'
+import HotelSearchPage from '../pages/HotelSearchPage'
 
 const AppRouter = () => {
 	return (
 		<div className='App'>
 			<Routes>
-				<Route path={'/'} element={<RequireAuth />} />
+				<Route
+					path=''
+					element={
+						<RequireAuth>
+							<HotelSearchPage />
+						</RequireAuth>
+					}
+				/>
+				<Route path={'/login'} element={<AuthPage />} />
 			</Routes>
 		</div>
 	)
